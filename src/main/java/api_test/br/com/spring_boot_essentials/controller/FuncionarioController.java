b33da546eb722093a28e9c7ba399bd876b361d08
Gustavo Henrique Ferreira Santos <gustavohenriqueferreirasantosf@gmail.com>
2026-05-04 21:25:05 -0300

    Bosta do gabriel e seu github

---
+++ src/main/java/api_test/br/com/spring_boot_essentials/controller/FuncionarioController.java
@@ -0,0 +1,7 @@
+package api_test.br.com.spring_boot_essentials.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FuncionarioController {
+}
